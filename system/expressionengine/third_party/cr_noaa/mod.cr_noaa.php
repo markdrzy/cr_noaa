f73a709a6cac5a29b1fc8b7530209daaed7c7b82
Mark Drzycimski <mark@clickrain.com>
2011-06-27 23:59:30 -0500

    Added zipcache table; Began work on current_conditions() method

--- system/expressionengine/third_party/cr_noaa/mod.cr_noaa.php
+++ system/expressionengine/third_party/cr_noaa/mod.cr_noaa.php
@@ -8,6 +8,26 @@ class Cr_noaa {
 	{
 		$this->EE =& get_instance();
 	}
+	
+	function current_conditions()
+	{
+		$zip = $this->EE->TMPL->fetch_param('zip');
+		$ll = $this->EE->TMPL->fetch_param('lat_lon')
+		if ($zip === FALSE && $ll === FALSE) return FALSE;
+		
+		if ($ll === FALSE)
+		{
+			$g = 'http://maps.google.com/maps/api/geocode/xml?address='.$zip.'&sensor=false';
+			$x = simplexml_load_file($g);
+			$ll = $x->result->geometry->location->lat.','.$x->result->geometry->location->lng;
+		}
+		
+		
+	}
+	
+	function forecast()
+	{
+	}
 
 }
 
--- system/expressionengine/third_party/cr_noaa/upd.cr_noaa.php
+++ system/expressionengine/third_party/cr_noaa/upd.cr_noaa.php
@@ -21,6 +21,18 @@ class Cr_noaa_upd {
 		);
 		$this->EE->db->insert('modules',$data);
 		
+		// Fire up the forge
+		$this->EE->load->dbforge();
+		
+		// Create Zipcache Table
+		$zipcache_fields = array(
+			'zip'			=> array('type' => 'int','constraint' => '10', 'unsigned' => TRUE),
+			'll'			=> array('type' => 'POINT')
+		);
+		$this->EE->dbforge->add_field($zipcache_fields);
+		$this->EE->dbforge->add_key('zip',TRUE);
+		$this->EE->dbforge->create_table($this->short_modname.'_zipcache');
+		
 		// Create Cache Directory
 		if ( ! mkdir(APPPATH . 'cache/' . $this->short_modname)) return FALSE;
 		
@@ -40,6 +52,12 @@ class Cr_noaa_upd {
 		$this->EE->db->delete('module_member_groups');
 		$this->EE->db->where('module_name',$this->modname);
 		$this->EE->db->delete('modules');
+		
+		// Fire up the forge
+		$this->EE->load->dbforge();
+		
+		// Drop Zipcache Table
+		$this->EE->dbforge->drop_table($this->mod_shortname.'_zipcache');
 		
 		// Delete Cache Files
 		foreach (glob('system/expressionengine/cache/cr_noaa/*.xml') as $f)
